cd63e2e01774dfac2815e326a64accafdde43438
KarunKumarV <kkarun701@gmail.com>
2024-03-25 12:37:19 +0530

    Create Main.java
    Initial commit

---
+++ JAVA/Main.java
@@ -0,0 +1,10 @@
+package JAVA;
+
+import java.util.Date;
+
+class Main{
+    public static void main(String[] args) {
+        System.out.println("this is for testing");
+        System.out.println(new Date());
+    }
+}
