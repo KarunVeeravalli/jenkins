733bc1eb578f50f9f82ce0303c2adfdd310118df
KarunVeeravalli <123941821+KarunVeeravalli@users.noreply.github.com>
2024-03-25 13:09:25 +0530

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,10 @@
+
+
+import java.util.Date;
+
+class Main{
+    public static void main(String[] args) {
+        System.out.println("this is for testing");
+        System.out.println(new Date());
+    }
+}
