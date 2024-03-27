f576a3e3e27df7abe9e0088576ca7c74c73faad7
KarunVeeravalli <123941821+KarunVeeravalli@users.noreply.github.com>
2024-03-27 11:20:44 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,6 +4,7 @@ import java.util.Date;
 
 class Main{
     public static void main(String[] args) {
+        System.out.println(new Date());
         System.out.println("this is for testing");
         System.out.println(new Date());
     }
